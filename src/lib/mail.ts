68f5bac508b2383477978e532575faf52a01a009
Prajwal-kp-18 <prajwal.kp.1817@gmail.com>
2024-11-29 11:14:10 +0530

    last one

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -13,7 +13,7 @@ export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   });
 };
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `https://kethi.vercel.app/auth/new-verification?token=${token}`;
+  const confirmLink = `https://khethideal.vercel.app/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
     from: "mail@khetideals.shop",
@@ -23,7 +23,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   });
 };
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetLink = `https://kethi.vercel.app/auth/new-password?token=${token}`;
+  const resetLink = `https://khethideal.vercel.app/auth/new-password?token=${token}`;
 
   await resend.emails.send({
     from: "mail@khetideals.shop",
